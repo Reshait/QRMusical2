6595f17caf87ed0085d01ba8a0e54128dac0fa53
Teófilo <p22romat@uco.es>
2017-09-02 02:58:58 +0200

    Improving css

--- appQRMusical/forms.py
+++ appQRMusical/forms.py
@@ -11,7 +11,19 @@ class UploadMultimediaForm(forms.ModelForm):
 		model = Multimedia
 		fields = ['file', 'image', 'players']
 
+	def __init__(self, *args, **kwargs):
+		super(UploadMultimediaForm, self).__init__(*args, **kwargs)
+		self.fields['file'].widget.attrs.update({'class' : 'form-control'})
+		self.fields['image'].widget.attrs.update({'class' : 'form-control btn btn-default btn-file'})
+		self.fields['players'].widget.attrs.update({'class' : 'selectpicker', 'multiple' : 'multiple'})
+
 class UploadPlayerForm(forms.ModelForm):	
 	class Meta:
 		model = Player
 		fields = ['name', 'description', 'enabled']
+
+	def __init__(self, *args, **kwargs):
+		super(UploadPlayerForm, self).__init__(*args, **kwargs)
+		self.fields['name'].widget.attrs.update({'class' : 'form-control'})
+		self.fields['description'].widget.attrs.update({'class' : 'form-control'})
+		self.fields['enabled'].widget.attrs.update({'class' : 'custom-control-input'})
